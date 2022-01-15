22533b543a17c3493da14171bd2eeb9d6b501d86
Kieran Emery <kizwiz@hotmail.co.uk>
2022-01-15 17:01:49 +0000

    KE-0001-BuildApplication: Deserialise JSON data
    Using the Newtonsoft.Json library package to deserialise the JSON data to extract the data we want (value / jokes). Created a class of properties based on the Chuck Norris Jokes API.

--- ChuckNorrisJokes/Program.cs
+++ ChuckNorrisJokes/Program.cs
@@ -19,8 +19,26 @@ namespace ChuckNorrisJokes
             string response = await client.GetStringAsync("https://api.chucknorris.io/jokes/random");
 
             Console.WriteLine(response);
+
+            var ChuckNorrisJoke = JsonConvert.DeserializeObject<ChuckNorrisJoke>(response);
+
+            Console.WriteLine(ChuckNorrisJoke);
+
             Console.ReadLine();
         }
 
     }
-}
+
+    /// <summary>
+    ///  Create a class of properties to deserialise the JSON data
+    /// </summary>
+    class ChuckNorrisJoke
+    {
+        public string[] categories { get; set; }
+        public DateTime created_at { get; set; }
+        public string icon_url { get; set; }
+        public string Id { get; set; }
+        public DateTime updated_at { get; set; }
+        public string url { get; set; }
+        public string value { get; set; }
+    }
