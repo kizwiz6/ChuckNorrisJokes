518798ff62f9ad2d0c3b6ead045cb4e54e8bc53e
Kieran Emery <kizwiz@hotmail.co.uk>
2022-01-16 15:42:54 +0000

    KE-0001-BuildApplicaiton: Outputs single joke & applies ReadKeys method
    Successfully deserialises the Chuck Norris API JSON data to retrieve the value of the joke on one line for the console application. Created a separate class for the Jokes get/set properties. Finally, working on a build for the ReadKeys method to get the characters pressed by the user.

---
+++ ChuckNorrisJokes/Jokes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChuckNorrisJokes
+{
+    /// <summary>
+    ///  Create a class of properties to deserialise the JSON data from the Chuck Norris jokes API.
+    /// </summary>
+
+    class Jokes
+    {
+        public string[] categories { get; set; }
+        public DateTime created_at { get; set; }
+        public string icon_url { get; set; }
+        public string Id { get; set; }
+        public DateTime updated_at { get; set; }
+        public string url { get; set; }
+        public string value { get; set; }
+    }
+}
--- ChuckNorrisJokes/Program.cs
+++ ChuckNorrisJokes/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace ChuckNorrisJokes
@@ -12,33 +13,85 @@ namespace ChuckNorrisJokes
         {
             Program program = new Program();
             await program.getChuckNorrisJokes();
+            ReadKeys();
         }
 
         private async Task getChuckNorrisJokes()
         {
             string response = await client.GetStringAsync("https://api.chucknorris.io/jokes/random");
 
-            Console.WriteLine(response);
+            var ChuckNorrisJoke = JsonConvert.DeserializeObject<Jokes>(response);
 
-            var ChuckNorrisJoke = JsonConvert.DeserializeObject<ChuckNorrisJoke>(response);
-
-            Console.WriteLine(ChuckNorrisJoke);
+            Console.WriteLine(ChuckNorrisJoke.value);
 
             Console.ReadLine();
         }
 
-    }
+        /// <summary>
+        /// ReadKeys method used for controlling the application by getting the characters pressed by the user.
+        /// </summary>
+        public static void ReadKeys()
 
-    /// <summary>
-    ///  Create a class of properties to deserialise the JSON data
-    /// </summary>
-    class ChuckNorrisJoke
-    {
-        public string[] categories { get; set; }
-        public DateTime created_at { get; set; }
-        public string icon_url { get; set; }
-        public string Id { get; set; }
-        public DateTime updated_at { get; set; }
-        public string url { get; set; }
-        public string value { get; set; }
+        {
+            /// ConsoleKey Info object assigned to the keyInfo variable
+            ConsoleKeyInfo keyInfo;
+            StringBuilder sb = new StringBuilder();
+
+            // Uses a do while loop
+            do
+            {
+                /// Reading the Console.ReadKey Method
+                keyInfo = Console.ReadKey();
+
+                /// Switch that switches on the key that was typed
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.A:
+                    case ConsoleKey.B:
+                    case ConsoleKey.C:
+                    case ConsoleKey.D:
+                    case ConsoleKey.E:
+                    case ConsoleKey.F:
+                    case ConsoleKey.G:
+                    case ConsoleKey.H:
+                    case ConsoleKey.I:
+                    case ConsoleKey.J:
+                    case ConsoleKey.K:
+                    case ConsoleKey.L:
+                    case ConsoleKey.M:
+                    case ConsoleKey.N:
+                    case ConsoleKey.O:
+                    case ConsoleKey.P:
+                    case ConsoleKey.Q:
+                    case ConsoleKey.R:
+                    case ConsoleKey.S:
+                    case ConsoleKey.T:
+                    case ConsoleKey.U:
+                    case ConsoleKey.V:
+                    case ConsoleKey.W:
+                    case ConsoleKey.X:
+                    case ConsoleKey.Y:
+                    case ConsoleKey.Z:
+                    case ConsoleKey.OemPeriod:
+                    case ConsoleKey.OemComma:
+                    case ConsoleKey.Spacebar:
+                        {
+                            sb.Append(keyInfo.KeyChar);
+                            break;
+                        }
+                    case ConsoleKey.Enter:
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine(sb.ToString());
+                            sb.Clear();
+                            break;
+                        }
+                    default: break;
+                }
+
+            } while (keyInfo.Key != ConsoleKey.Escape);
+
+
+        }
     }
+}
